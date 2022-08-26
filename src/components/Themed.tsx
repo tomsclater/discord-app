7a8976054cae6a17d26673bd4cbb029dc538fada
tomsclater <tomsclater@live.com>
2022-08-25 23:30:28 -0700

    Moved components location

--- src/components/Themed.tsx
+++ src/components/Themed.tsx
@@ -5,8 +5,8 @@
 
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
-import Colors from '../src/constants/Colors';
-import useColorScheme from '../src/hooks/useColorScheme';
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
