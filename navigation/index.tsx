c8497dd8cdbd1504c88d3b1cd284a60babd691ad
tomsclater <tomsclater@live.com>
2022-08-25 23:21:34 -0700

    Moved screens location

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -12,9 +12,9 @@ import { ColorSchemeName, Pressable } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
+import NotFoundScreen from '../src/screens/NotFoundScreen';
+import TabOneScreen from '../src/screens/TabOneScreen';
+import TabTwoScreen from '../src/screens/TabTwoScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
