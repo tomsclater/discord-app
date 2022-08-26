78e958c5c25249a5ba28b865663239503de7ddae
tomsclater <tomsclater@live.com>
2022-08-25 23:26:46 -0700

    Moved hooks location

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -6,7 +6,7 @@
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
+import useColorScheme from '../src/hooks/useColorScheme';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -11,7 +11,7 @@ import * as React from 'react';
 import { ColorSchemeName, Pressable } from 'react-native';
 
 import Colors from '../../constants/Colors';
-import useColorScheme from '../../hooks/useColorScheme';
+import useColorScheme from '../hooks/useColorScheme';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
